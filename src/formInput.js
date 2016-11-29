6cadf21b24f63f8617f332e87e1dd40644f8313e
Tanner Linsley <tannerlinsley@gmail.com>
2016-11-29 15:05:42 -0700

    NestedForm has no -hasError class

--- src/formInput.js
+++ src/formInput.js
@@ -4,23 +4,23 @@ import classnames from 'classnames'
 import FormField from './formField'
 import FormError from './formError'
 
-export default function FormInput ({ field, showErrors = true, errorBefore, children }) {
+export default function FormInput ({ field, showErrors = true, isForm, children }) {
   return (
     <FormField field={field}>
       {({ ...api }) => {
         const classes = classnames('FormInput', {
-          '-hasError': showErrors && api.getTouched() && api.getError()
+          '-hasError': !isForm && showErrors && api.getTouched() && api.getError()
         })
 
         return (
           <div className={classes}>
-            {errorBefore && showErrors && (
+            {isForm && showErrors && (
               <FormError field={field} />
             )}
             {children({
               ...api
             })}
-            {!errorBefore && showErrors && (
+            {!isForm && showErrors && (
               <FormError field={field} />
             )}
           </div>
