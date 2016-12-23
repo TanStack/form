3d7d3fa509addfc926782e83c4f0b32b920b0fd0
ali.q <ghanavatian.ali@gmail.com>
2016-12-23 17:35:13 +0330

    add storybook
    add storybook

---
+++ stories/SimpleText.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Form from '../src/form'
+import Text from '../src/formInputs/text'
+
+const SimpleText = Form({
+  validate: ({ name }) => {
+    return {
+      name: !name ? 'A name is required' : undefined
+    }
+  }
+})(({submitForm}) => {
+  return (
+    <form onSubmit={submitForm}>
+      <p>Submit event would have 3 objects: values, state and props.</p>
+      <label>name:*</label>
+      <Text field='name' placeholder='type something...' />
+      <pre>By default validation performs on <b>blur</b> and <b>submit</b> events</pre>
+      <button type='submit'>Submit</button>
+    </form>
+  )
+})
+
+export default SimpleText
