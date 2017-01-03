b9a62348383f61a71ee3673fce5debc7e2f44791
ali.q <ghanavatian.ali@gmail.com>
2017-01-03 13:44:12 +0330

    replace standard with standard-jsx

--- stories/DemoForm.js
+++ stories/DemoForm.js
@@ -14,23 +14,23 @@ const FormComponent = ({ values, submitForm, addValue, removeValue, getError }) 
       <div>
         <h6>Full Name</h6>
         <Text // This is the built-in Text formInput
-          field='name' // field is a string version of the field location
-          placeholder='Your name' // all other props are sent through to the underlying component, in this case an <input />
+          field="name" // field is a string version of the field location
+          placeholder="Your name" // all other props are sent through to the underlying component, in this case an <input />
         />
       </div>
 
       <div>
         <h6>Hobby</h6>
         <Text // This is the built-in Text formInput
-          field='hobby' // field is a string version of the field location
-          placeholder='Your hobbies' // all other props are sent through to the underlying component, in this case an <input />
+          field="hobby" // field is a string version of the field location
+          placeholder="Your hobbies" // all other props are sent through to the underlying component, in this case an <input />
         />
       </div>
 
       <div>
         <h6>Relationship Status</h6>
         <Select // This is the built-in Select formInput
-          field='status'
+          field="status"
           options={[{ // You can ship it some options like usual
             label: 'Single',
             value: 'single'
@@ -47,16 +47,16 @@ const FormComponent = ({ values, submitForm, addValue, removeValue, getError }) 
       <div>
         <h6>Short Bio</h6>
         <FormInputTextarea // This is the built-in Textarea formInput
-          field='bio'
-          placeholder='Short Bio'
+          field="bio"
+          placeholder="Short Bio"
         />
       </div>
 
       {/* Arrays in forms are super easy to handle */}
       <h6>Friends</h6>
       {/* This is a custom form error for the root of the friends list (see validation function) */}
-      <FormError field='friends' />
-      <div className='nested'>
+      <FormError field="friends" />
+      <div className="nested">
         {!values.friends.length ? (
           <em>No friends have been added yet</em>
         ) : values.friends.map((friends, i) => ( // Loop over the values however you'd like
@@ -66,7 +66,7 @@ const FormComponent = ({ values, submitForm, addValue, removeValue, getError }) 
               <h6>Full Name</h6>
               <Text
                 field={['friends', i, 'name']} // You can easily pass an array-style field path. Perfect for passing down as props or nested values
-                placeholder='Friend Name'
+                placeholder="Friend Name"
               />
             </div>
 
@@ -88,7 +88,7 @@ const FormComponent = ({ values, submitForm, addValue, removeValue, getError }) 
             </div>
 
             <button // This button will remove this friend from the `friends` field
-              type='button'
+              type="button"
               onClick={() => removeValue('friends', i)} // `removeValue` takes a field location for an array, and the index for the item to remove
             >
               Remove Friend
@@ -100,7 +100,7 @@ const FormComponent = ({ values, submitForm, addValue, removeValue, getError }) 
 
       <div>
         <button // This button will add a new blank friend item to the `friends` field
-          type='button'
+          type="button"
           onClick={() => addValue('friends', {})} // `addValue` takes an array-like field, and the value to add
         >
           Add Friend
@@ -110,7 +110,7 @@ const FormComponent = ({ values, submitForm, addValue, removeValue, getError }) 
       <div>
         <label>
           <Checkbox // This is the built-in checkbox formInput
-            field='createAccount'
+            field="createAccount"
           />
           <span>Create Account?</span>
         </label>
@@ -121,22 +121,22 @@ const FormComponent = ({ values, submitForm, addValue, removeValue, getError }) 
         <radiogroup>
           <label>
             <Radio // This is the built-in radio formInput
-              field='notificationType'
-              value='email' // This is the value the field will be set to when this radio button is active
+              field="notificationType"
+              value="email" // This is the value the field will be set to when this radio button is active
             />
             <span>Email</span>
           </label>
           <label>
             <Radio
-              field='notificationType'
-              value='text'
+              field="notificationType"
+              value="text"
             />
             <span>Text</span>
           </label>
           <label>
             <Radio
-              field='notificationType'
-              value='phone'
+              field="notificationType"
+              value="phone"
             />
             <span>Phone</span>
           </label>
