0e933ce1a94d52653035cd6441433d80d96fa9f5
Shane Keulen <shane.keulen@gmail.com>
2017-02-07 11:38:00 -0500

    squelch react warning

--- src/formInputs/radio.js
+++ src/formInputs/radio.js
@@ -1,19 +1,21 @@
 import React from 'react'
 //
 import { buildHandler } from './util'
+const noop = () => {}
 
 export default React.createClass({
   contextTypes: {
     formRadioGroup: React.PropTypes.object
   },
   render () {
-    const { value, onClick, onBlur, ...rest } = this.props
+    const { value, onClick, onChange, onBlur, ...rest } = this.props
     const {setValue, getValue, setTouched, props: { noTouch }} = this.context.formRadioGroup
     return (
       <input
         {...rest}
         type='radio'
         checked={getValue(false) === value}
+        onChange={buildHandler(onChange, noop)}
         onClick={buildHandler(onClick, e => setValue(value, noTouch))}
         onBlur={buildHandler(onBlur, () => setTouched())}
       />
