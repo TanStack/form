1630497bba7de2dee22d72c7e5761eb6b64f1b44
zalishchuk <zalishchuk.me@ya.ru>
2017-12-08 00:34:34 +0200

    Remove webpack-node-externals

--- config/webpack/webpack.dist.config.js
+++ config/webpack/webpack.dist.config.js
@@ -1,12 +1,40 @@
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
+// const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: [
     path.join(__dirname, '../../src/index.js')
   ],
-  target: 'node', // in order to ignore built-in modules like path, fs, etc.
-  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  // target: 'node', // in order to ignore built-in modules like path, fs, etc.
+  // externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React'
+    },
+    // Uncomment redux and react-redux section if dont want to bundle them into dist
+
+    // redux: {
+    //   commonjs: 'redux',
+    //   commonjs2: 'redux',
+    //   amd: 'redux',
+    //   root: 'Redux'
+    // },
+    // 'react-redux': {
+    //   commonjs: 'react-redux',
+    //   commonjs2: 'react-redux',
+    //   amd: 'react-redux',
+    //   root: 'ReactRedux'
+    // },
+    'prop-types': {
+      commonjs: 'prop-types',
+      commonjs2: 'prop-types',
+      amd: 'prop-types',
+      root: 'PropTypes'
+    }
+  },
   module: {
     rules: [
       {
