568c9f5928032429de21d14c27905390a0ddff8f
Christophe Escobar <christophe.esco@gmail.com>
2018-01-11 15:37:58 +0100

    chore: Radio input does not have warning with bool value

--- src/components/form-fields/Radio.js
+++ src/components/form-fields/Radio.js
@@ -46,7 +46,7 @@ class Radio extends Component {
 }
 
 Radio.propTypes = {
-  value: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]).isRequired,
   group: PropTypes.object.isRequired
 };
 
