6d9d4e776791dcb0ebb5435ce4d9e2f4a7b92f1c
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2019-12-02 17:26:08 +0530

    setup size-plugin-bot

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,29 @@
-import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
-import external from "rollup-plugin-peer-deps-external";
-import resolve from "rollup-plugin-node-resolve";
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import external from 'rollup-plugin-peer-deps-external'
+import resolve from 'rollup-plugin-node-resolve'
+import size from 'rollup-plugin-size';
+import pkg from './package.json'
 
-import pkg from "./package.json";
-
-export default {
-  input: "src/index.js",
-  output: [
-    {
+export default [
+  {
+    input: 'src/index.js',
+    output: {
       file: pkg.main,
-      format: "cjs",
-      sourcemap: true
+      format: 'cjs',
+      sourcemap: true,
     },
-    {
+    plugins: [external(), babel(), resolve(), commonjs(),
+      size({publish:true,exclude:pkg.module,filename:'sizes-cjs.json',writeFile:process.env.CI?true:false})],
+  },
+  {
+    input: 'src/index.js',
+    output: {
       file: pkg.module,
-      format: "es",
-      sourcemap: true
-    }
-  ],
-  plugins: [ 
-    external(),
-    babel(),
-    resolve(),
-    commonjs()
-  ]
-};
+      format: 'es',
+      sourcemap: true,
+    },
+    plugins: [external(), babel(),
+      size({publish:true,exclude:pkg.main,filename:'sizes-es.json',writeFile:process.env.CI?true:false})],
+  },
+]
