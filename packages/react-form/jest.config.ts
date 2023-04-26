b167bdced9b3a33c3b70abfc73d4d0239ea348d0
Zack DeRose <zack.derose@gmail.com>
2023-04-25 19:18:40 -0700

    fixing jest config

--- packages/react-form/jest.config.ts
+++ packages/react-form/jest.config.ts
@@ -1,6 +1,5 @@
 export default {
   displayName: 'react-form',
   preset: '../../jest-preset.js',
-  setupFilesAfterEnv: ['./jest.setup.ts'],
   testMatch: ['<rootDir>/src/**/*.test.tsx', '<rootDir>/codemods/**/*.test.js'],
 }
