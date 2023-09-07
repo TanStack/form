320712e3082b6f5a2af802e4776718ad5e1d5fde
Corbin Crutchley <git@crutchcorn.dev>
2023-09-07 16:12:07 -0700

    test: add useField Vue tests

--- packages/vue-form/src/tests/useField.test.tsx
+++ packages/vue-form/src/tests/useField.test.tsx
@@ -9,6 +9,8 @@ import {
   useForm,
 } from '../index'
 import userEvent from '@testing-library/user-event'
+import * as React from 'react'
+import { sleep } from './utils'
 
 const user = userEvent.setup()
 
@@ -45,5 +47,194 @@ describe('useField', () => {
     const { getByTestId } = render(Comp)
     const input = getByTestId('fieldinput')
     await waitFor(() => expect(input).toHaveValue('FirstName'))
+  })
+
+  it('should not validate on change if isTouched is false', async () => {
+    type Person = {
+      firstName: string
+      lastName: string
+    }
+    const error = 'Please enter a different value'
+
+    const formFactory = createFormFactory<Person>()
+
+    const Comp = defineComponent(() => {
+      const form = formFactory.useForm()
+
+      provideFormContext({ formApi: form })
+
+      return () => (
+        <form.Field
+          name="firstName"
+          onChange={(value) => (value === 'other' ? error : undefined)}
+        >
+          {(field: FieldApi<string, Person>) => (
+            <div>
+              <input
+                data-testid="fieldinput"
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onInput={(e) =>
+                  field.setValue((e.target as HTMLInputElement).value)
+                }
+              />
+              <p>{field.getMeta().error}</p>
+            </div>
+          )}
+        </form.Field>
+      )
+    })
+
+    const { getByTestId, queryByText } = render(Comp)
+    const input = getByTestId('fieldinput')
+    await user.type(input, 'other')
+    expect(queryByText(error)).not.toBeInTheDocument()
+  })
+
+  it('should validate on change if isTouched is true', async () => {
+    type Person = {
+      firstName: string
+      lastName: string
+    }
+    const error = 'Please enter a different value'
+
+    const formFactory = createFormFactory<Person>()
+
+    const Comp = defineComponent(() => {
+      const form = formFactory.useForm()
+
+      provideFormContext({ formApi: form })
+
+      return () => (
+        <form.Field
+          name="firstName"
+          onChange={(value) => (value === 'other' ? error : undefined)}
+        >
+          {(field: FieldApi<string, Person>) => (
+            <div>
+              <input
+                data-testid="fieldinput"
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onInput={(e) =>
+                  field.handleChange((e.target as HTMLInputElement).value)
+                }
+              />
+              <p>{field.getMeta().error}</p>
+            </div>
+          )}
+        </form.Field>
+      )
+    })
+
+    const { getByTestId, getByText, queryByText } = render(Comp)
+    const input = getByTestId('fieldinput')
+    expect(queryByText(error)).not.toBeInTheDocument()
+    await user.type(input, 'other')
+    expect(getByText(error)).toBeInTheDocument()
+  })
+
+  it('should validate async on change', async () => {
+    type Person = {
+      firstName: string
+      lastName: string
+    }
+    const error = 'Please enter a different value'
+
+    const formFactory = createFormFactory<Person>()
+
+    const Comp = defineComponent(() => {
+      const form = formFactory.useForm()
+
+      provideFormContext({ formApi: form })
+
+      return () => (
+        <form.Field
+          name="firstName"
+          defaultMeta={{ isTouched: true }}
+          onChangeAsync={async () => {
+            await sleep(10)
+            return error
+          }}
+        >
+          {(field: FieldApi<string, Person>) => (
+            <div>
+              <input
+                data-testid="fieldinput"
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onInput={(e) =>
+                  field.handleChange((e.target as HTMLInputElement).value)
+                }
+              />
+              <p>{field.getMeta().error}</p>
+            </div>
+          )}
+        </form.Field>
+      )
+    })
+
+    const { getByTestId, getByText, queryByText } = render(Comp)
+    const input = getByTestId('fieldinput')
+    expect(queryByText(error)).not.toBeInTheDocument()
+    await user.type(input, 'other')
+    await waitFor(() => getByText(error))
+    expect(getByText(error)).toBeInTheDocument()
+  })
+
+  it('should validate async on change with debounce', async () => {
+    type Person = {
+      firstName: string
+      lastName: string
+    }
+
+    const mockFn = vi.fn()
+    const error = 'Please enter a different value'
+    const formFactory = createFormFactory<Person>()
+
+    const Comp = defineComponent(() => {
+      const form = formFactory.useForm()
+
+      provideFormContext({ formApi: form })
+
+      return () => (
+        <form.Field
+          name="firstName"
+          defaultMeta={{ isTouched: true }}
+          onChangeAsyncDebounceMs={100}
+          onChangeAsync={async () => {
+            mockFn()
+            await sleep(10)
+            return error
+          }}
+        >
+          {(field: FieldApi<string, Person>) => (
+            <div>
+              <input
+                data-testid="fieldinput"
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onInput={(e) =>
+                  field.handleChange((e.target as HTMLInputElement).value)
+                }
+              />
+              <p>{field.getMeta().error}</p>
+            </div>
+          )}
+        </form.Field>
+      )
+    })
+
+    const { getByTestId, getByText } = render(<Comp />)
+    const input = getByTestId('fieldinput')
+    await user.type(input, 'other')
+    // mockFn will have been called 5 times without onChangeAsyncDebounceMs
+    expect(mockFn).toHaveBeenCalledTimes(0)
+    await waitFor(() => getByText(error))
+    expect(getByText(error)).toBeInTheDocument()
   })
 })
