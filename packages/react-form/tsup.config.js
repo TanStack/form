42e0237f44b904331ccd653cc1c755edfac10894
Corbin Crutchley <git@crutchcorn.dev>
2023-09-07 19:45:39 -0700

    fix: bundler errors in React and Vue adapters should now be resolved

--- packages/react-form/tsup.config.js
+++ packages/react-form/tsup.config.js
@@ -4,6 +4,10 @@ import { defineConfig } from 'tsup'
 import { legacyConfig, modernConfig } from '../../scripts/getTsupConfig.js'
 
 export default defineConfig([
-  modernConfig({ entry: ['src/*.ts', 'src/*.tsx'] }),
-  legacyConfig({ entry: ['src/*.ts', 'src/*.tsx'] }),
+  modernConfig({
+    entry: ['src/*.ts', 'src/*.tsx', 'src/utils/*.ts', 'src/utils/*.tsx'],
+  }),
+  legacyConfig({
+    entry: ['src/*.ts', 'src/*.tsx', 'src/utils/*.ts', 'src/utils/*.tsx'],
+  }),
 ])
