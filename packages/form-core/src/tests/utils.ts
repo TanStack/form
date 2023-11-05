2835cc33aa1b3326e88383064416a7974eccb0cc
Christian Siebmanns <christian.siebmanns@hotmail.de>
2023-11-05 21:30:25 +0100

    Add tests for utils

--- packages/form-core/src/tests/utils.ts
+++ packages/form-core/src/tests/utils.ts
@@ -3,3 +3,18 @@ export function sleep(timeout: number): Promise<void> {
     setTimeout(resolve, timeout)
   })
 }
+
+interface Kid {
+  name: string;
+  age: number
+}
+export interface Person {
+  name: string
+  kids: Kid[]
+  mother: {name: string}
+}
+export function makeComplexFormStructure(): Person {
+  return {name: 'Marc', kids: [{name: 'Stephen', age: 10}, {name: 'Taylor', age: 15}], mother: {
+    name: 'Lisa'
+    }};
+}
