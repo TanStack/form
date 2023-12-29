4adba6697bc9aacf4a2718db02402e16dd25ddbd
Corbin Crutchley <git@crutchcorn.dev>
2023-12-28 21:11:44 -0700

    chore: refactor to single config generator

--- packages/react-form/vite.config.ts
+++ packages/react-form/vite.config.ts
@@ -1,59 +1,16 @@
-import { defineConfig } from 'vitest/config'
-import { externalizeDeps } from 'vite-plugin-externalize-deps'
-import dts from 'vite-plugin-dts'
-import { copyFileSync } from 'node:fs'
+import { defineConfig, mergeConfig } from 'vitest/config'
+import { defaultViteConfig } from '../../getViteConfig'
 
-export default defineConfig({
-  plugins: [
-    dts({
-      outDir: 'dist/mjs',
-      afterBuild: () => {
-        // To pass publint (`npm x publint@latest`) and ensure the
-        // package is supported by all consumers, we must export types that are
-        // read as ESM. To do this, there must be duplicate types with the
-        // correct extension supplied in the package.json exports field.
-        copyFileSync('dist/mjs/index.d.ts', 'dist/mjs/index.d.mts')
-      },
-      compilerOptions: {
-        module: 'esnext',
-      },
-    }),
-    dts({
-      outDir: 'dist/cjs',
-      afterBuild: () => {
-        copyFileSync('dist/cjs/index.d.ts', 'dist/cjs/index.d.cts')
-      },
-      compilerOptions: {
-        module: 'commonjs',
-      },
-    }),
-    externalizeDeps(),
-    {
-      name: 'copy-mjs-cjs-to-index',
-      closeBundle() {
-        copyFileSync('dist/mjs/index.mjs', 'dist/mjs/index.js')
-        copyFileSync('dist/cjs/index.cjs', 'dist/cjs/index.js')
-      },
+export default mergeConfig(
+  defaultViteConfig,
+  defineConfig({
+    test: {
+      name: 'react-form',
+      dir: './src',
+      watch: false,
+      environment: 'jsdom',
+      globals: true,
+      coverage: { provider: 'istanbul' },
     },
-  ],
-  build: {
-    minify: false,
-    sourcemap: true,
-    lib: {
-      entry: 'src/index.ts',
-      formats: ['es', 'cjs'],
-      fileName: (format) => {
-        if (format === 'cjs') return `cjs/index.cjs`
-        return `mjs/index.mjs`
-      },
-    },
-  },
-  test: {
-    name: 'react-form',
-    dir: './src',
-    watch: false,
-    environment: 'jsdom',
-    globals: true,
-    coverage: { provider: 'istanbul' },
-  },
-})
+  }),
+)
