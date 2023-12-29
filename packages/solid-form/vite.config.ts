cfe9e3baf95f72d06745e7ed6108025f1d9a885c
Corbin Crutchley <git@crutchcorn.dev>
2023-12-28 21:04:25 -0700

    chore: migrate solid and react packages to use vite

---
+++ packages/solid-form/vite.config.ts
@@ -0,0 +1,68 @@
+import { defineConfig } from 'vitest/config'
+import { externalizeDeps } from 'vite-plugin-externalize-deps'
+import dts from 'vite-plugin-dts'
+import { copyFileSync } from 'node:fs'
+import solid from 'vite-plugin-solid'
+
+export default defineConfig({
+  plugins: [
+    solid(),
+    dts({
+      outDir: 'dist/mjs',
+      afterBuild: () => {
+        // To pass publint (`npm x publint@latest`) and ensure the
+        // package is supported by all consumers, we must export types that are
+        // read as ESM. To do this, there must be duplicate types with the
+        // correct extension supplied in the package.json exports field.
+        copyFileSync('dist/mjs/index.d.ts', 'dist/mjs/index.d.mts')
+      },
+      compilerOptions: {
+        module: 'esnext',
+      },
+    }),
+    dts({
+      outDir: 'dist/cjs',
+      afterBuild: () => {
+        copyFileSync('dist/cjs/index.d.ts', 'dist/cjs/index.d.cts')
+      },
+      compilerOptions: {
+        module: 'commonjs',
+      },
+    }),
+    externalizeDeps(),
+    {
+      name: 'copy-mjs-cjs-to-index',
+      closeBundle() {
+        copyFileSync('dist/mjs/index.mjs', 'dist/mjs/index.js')
+        copyFileSync('dist/cjs/index.cjs', 'dist/cjs/index.js')
+      },
+    },
+  ],
+  build: {
+    minify: false,
+    sourcemap: true,
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es', 'cjs'],
+      fileName: (format) => {
+        if (format === 'cjs') return `cjs/index.cjs`
+        return `mjs/index.mjs`
+      },
+    },
+  },
+  test: {
+    name: 'solid-form',
+    dir: './src',
+    watch: false,
+    setupFiles: [],
+    environment: 'jsdom',
+    globals: true,
+    coverage: { provider: 'istanbul' },
+    server: {
+      deps: {
+        // https://github.com/solidjs/solid-testing-library#known-issues
+        inline: [/solid-js/],
+      },
+    },
+  },
+})
