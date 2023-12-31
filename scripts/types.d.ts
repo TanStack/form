2a5181d7463bb49fcbf35179ce0f63b1e50152b7
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-12-31 11:06:23 +1100

    Initial rename and copy

--- scripts/types.d.ts
+++ scripts/types.d.ts
@@ -20,16 +20,16 @@ export type AuthorOrCommitter = {
 }
 
 export type Parsed = {
-  type: string
+  type: string | null
   scope?: string | null
   subject: string
   merge?: null
   header: string
   body?: null
   footer?: null
-  notes?: null[] | null
-  references?: null[] | null
-  mentions?: null[] | null
+  notes?: Array<null> | null
+  references?: Array<null> | null
+  mentions?: Array<null> | null
   revert?: null
   raw: string
 }
@@ -41,5 +41,5 @@ export type Package = {
 
 export type BranchConfig = {
   prerelease: boolean
-  ghRelease: boolean
+  previousVersion?: boolean
 }
