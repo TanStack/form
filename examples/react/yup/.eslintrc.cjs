ea574f52563daf2396d5569cfd42638efd9d394a
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-02-04 08:25:01 +1100

    Remove unnecessary parserOptions

--- examples/react/yup/.eslintrc.cjs
+++ examples/react/yup/.eslintrc.cjs
@@ -3,10 +3,6 @@
 /** @type {import('eslint').Linter.Config} */
 const config = {
   extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended'],
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: true,
-  },
   rules: {
     'react/no-children-prop': 'off',
   },
