709167cefee9371a72121bb30867a8763db2ef30
Corbin Crutchley <git@crutchcorn.dev>
2024-03-04 18:21:36 -0800

    chore: add test to React array usage

--- packages/react-form/src/tests/useField.test.tsx
+++ packages/react-form/src/tests/useField.test.tsx
@@ -608,4 +608,84 @@ describe('useField', () => {
     await user.click(getByText('Submit'))
     expect(queryByText('A first name is required')).not.toBeInTheDocument()
   })
+
+  it('should handle arrays with subvalues', async () => {
+    const fn = vi.fn()
+    function Comp() {
+      const form = useForm({
+        defaultValues: {
+          people: [] as Array<{ age: number; name: string }>,
+        },
+        onSubmit: ({ value }) => fn(value),
+      })
+
+      return (
+        <div>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault()
+              e.stopPropagation()
+              void form.handleSubmit()
+            }}
+          >
+            <form.Field name="people">
+              {(field) => {
+                return (
+                  <div>
+                    {field.state.value.map((_, i) => {
+                      return (
+                        <form.Field key={i} name={`people[${i}].name`}>
+                          {(subField) => {
+                            return (
+                              <div>
+                                <label>
+                                  <div>Name for person {i}</div>
+                                  <input
+                                    value={subField.state.value}
+                                    onChange={(e) =>
+                                      subField.handleChange(e.target.value)
+                                    }
+                                  />
+                                </label>
+                              </div>
+                            )
+                          }}
+                        </form.Field>
+                      )
+                    })}
+                    <button
+                      onClick={() => field.pushValue({ name: '', age: 0 })}
+                      type="button"
+                    >
+                      Add person
+                    </button>
+                  </div>
+                )
+              }}
+            </form.Field>
+            <form.Subscribe
+              selector={(state) => [state.canSubmit, state.isSubmitting]}
+              children={([canSubmit, isSubmitting]) => (
+                <button type="submit" disabled={!canSubmit}>
+                  {isSubmitting ? '...' : 'Submit'}
+                </button>
+              )}
+            />
+          </form>
+        </div>
+      )
+    }
+
+    const { getByText, findByLabelText, queryByText, findByText } = render(
+      <Comp />,
+    )
+
+    expect(queryByText('Name for person 0')).not.toBeInTheDocument()
+    await user.click(getByText('Add person'))
+    const input = await findByLabelText('Name for person 0')
+    expect(input).toBeInTheDocument()
+    await user.type(input, 'John')
+    await user.click(await findByText('Submit'))
+    expect(fn).toHaveBeenCalledWith({ people: [{ name: 'John', age: 0 }] })
+  })
 })
