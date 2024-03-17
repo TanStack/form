ba7bf87bc46f1bdfc2114dfce173c2f7b16a52ec
Corbin Crutchley <git@crutchcorn.dev>
2024-03-17 00:30:53 -0600

    chore: more work on adapter

--- examples/angular/simple/src/app/app.component.ts
+++ examples/angular/simple/src/app/app.component.ts
@@ -1,25 +1,29 @@
 import { Component } from '@angular/core'
 import { injectForm, TanStackField } from '@tanstack/angular-form'
+import { JsonPipe, NgFor } from '@angular/common'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TanStackField],
+  imports: [TanStackField, NgFor, JsonPipe],
   template: `
     <p>Testing</p>
     <form (submit)="handleSubmit($event)">
       <ng-template
         [tanstackField]="form"
-        [fieldOpts]="{ name: 'firstName' }"
+        name="firstName"
+        [validators]="{ onChange: required }"
         let-field
       >
         <label>
           <div>First name:</div>
+          <p>{{ field | json }}</p>
           <input
             [value]="field.state.value"
             (blur)="field.handleBlur()"
             (input)="field.handleChange($any($event).target.value)"
           />
+          <div *ngFor="let error of field.state.meta.errors">{{ error }}</div>
         </label>
       </ng-template>
       <button>Submit</button>
@@ -27,9 +31,14 @@ import { injectForm, TanStackField } from '@tanstack/angular-form'
   `,
 })
 export class AppComponent {
+  required = (props: any) => {
+    console.log(props)
+    return !props.value ? 'Required' : undefined
+  }
   form = injectForm({
     defaultValues: {
       firstName: 'Ryan',
+      age: 25,
     },
     onSubmit({ value }: any) {
       console.log({ value })
--- packages/angular-form/src/tanstack-field.directive.ts
+++ packages/angular-form/src/tanstack-field.directive.ts
@@ -1,7 +1,7 @@
 import {
   Directive,
   inject,
-  Input,
+  Input, OnChanges,
   OnInit,
   TemplateRef,
   ViewContainerRef,
@@ -10,8 +10,11 @@ import {
   type DeepKeys,
   type DeepValue,
   FieldApi,
-  FieldOptions,
+  type FieldMeta,
+  type FieldOptions,
+  type FieldValidators,
   FormApi,
+  type NoInfer,
   type Validator,
 } from '@tanstack/form-core'
 
@@ -20,40 +23,69 @@ import {
   standalone: true,
 })
 export class TanStackField<
-  TParentData,
-  TName extends DeepKeys<TParentData>,
-  TFieldValidator extends
-    | Validator<DeepValue<TParentData, TName>, unknown>
-    | undefined = undefined,
-  TFormValidator extends
-    | Validator<TParentData, unknown>
-    | undefined = undefined,
-  TData extends DeepValue<TParentData, TName> = DeepValue<TParentData, TName>,
-> implements OnInit
+    TParentData,
+    TName extends DeepKeys<TParentData>,
+    TFieldValidator extends
+      | Validator<DeepValue<TParentData, TName>, unknown>
+      | undefined = undefined,
+    TFormValidator extends
+      | Validator<TParentData, unknown>
+      | undefined = undefined,
+    TData extends DeepValue<TParentData, TName> = DeepValue<TParentData, TName>,
+  >
+  implements
+    OnInit,
+    OnChanges,
+    FieldOptions<TParentData, TName, TFieldValidator, TFormValidator, TData>
 {
+  @Input({ required: true }) name!: TName
+  @Input() defaultValue?: NoInfer<TData>
+  @Input() asyncDebounceMs?: number
+  @Input() asyncAlways?: boolean
+  @Input() preserveValue?: boolean
+  @Input() validatorAdapter?: TFieldValidator
   @Input({ required: true }) tanstackField!: FormApi<
     TParentData,
     TFormValidator
   >
-  @Input({ required: true }) fieldOpts!: FieldOptions<
+  @Input() validators?: FieldValidators<
     TParentData,
     TName,
     TFieldValidator,
     TFormValidator,
     TData
   >
+  @Input() defaultMeta?: Partial<FieldMeta>
 
   template = inject(TemplateRef)
   viewContainer = inject(ViewContainerRef)
 
-  ngOnInit() {
-    const api = new FieldApi({
-      ...this.fieldOpts,
+  api!: FieldApi<TParentData, TName, TFieldValidator, TFormValidator, TData>
+
+  private getOptions() {
+    return {
+      defaultValue: this.defaultValue,
+      asyncDebounceMs: this.asyncDebounceMs,
+      asyncAlways: this.asyncAlways,
+      preserveValue: this.preserveValue,
+      validatorAdapter: this.validatorAdapter,
+      validators: this.validators,
+      defaultMeta: this.defaultMeta,
+      name: this.name,
       form: this.tanstackField,
-    })
+    }
+  }
+
+  ngOnInit() {
+    this.api = new FieldApi(this.getOptions())
 
     this.viewContainer.createEmbeddedView(this.template, {
-      $implicit: api,
+      $implicit: this.api,
     })
+  }
+
+  ngOnChanges() {
+    if (!this.api) return;
+    this.api.update(this.getOptions())
   }
 }
