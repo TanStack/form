f2ca3c4ae4c2b10041574e1031335e858ca8881f
Corbin Crutchley <git@crutchcorn.dev>
2024-03-18 02:26:20 -0600

    chore: WIP fixing CI

--- examples/angular/simple/src/main.ts
+++ examples/angular/simple/src/main.ts
@@ -1,5 +1,4 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser'
+import { AppComponent } from './app/app.component'
 
-bootstrapApplication(AppComponent)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent).catch((err) => console.error(err))
