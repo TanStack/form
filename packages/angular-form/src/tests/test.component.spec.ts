7700c3f145c3e40871d6073712b174ef4636999a
Corbin Crutchley <git@crutchcorn.dev>
2024-03-18 13:35:19 -0600

    test: add tests for validating

--- packages/angular-form/src/tests/test.component.spec.ts
+++ packages/angular-form/src/tests/test.component.spec.ts
@@ -1,8 +1,12 @@
 import { render } from '@testing-library/angular'
 import { Component } from '@angular/core'
 import { describe, expect, it } from 'vitest'
+import { userEvent } from '@testing-library/user-event'
 import { injectForm } from '../inject-form'
 import { TanStackField } from '../tanstack-field.directive'
+import type { FieldValidateFn } from '@tanstack/form-core'
+
+const user = userEvent.setup()
 
 describe('TanStackFieldDirective', () => {
   it('should allow to set default value', async () => {
@@ -75,5 +79,102 @@ describe('TanStackFieldDirective', () => {
 
     const input = getByTestId('fieldinput')
     expect(input).toHaveValue('otherName')
+  })
+
+  it('should not validate on change if isTouched is false', async () => {
+    type Person = {
+      firstName: string
+      lastName: string
+    }
+    const error = 'Please enter a different value'
+
+    @Component({
+      selector: 'test-component',
+      standalone: true,
+      template: `
+        <ng-template
+          [tanstackField]="form"
+          name="firstName"
+          [validators]="{ onChange: otherValidator }"
+          let-field
+        >
+          <input
+            data-testid="fieldinput"
+            [value]="field.state.value"
+            (blur)="field.handleBlur()"
+            (input)="field.setValue($any($event).target.value)"
+          />
+          @for (error of field.getMeta().errors; track error) {
+            <p>{{ error }}</p>
+          }
+        </ng-template>
+      `,
+      imports: [TanStackField],
+    })
+    class TestComponent {
+      otherValidator: FieldValidateFn<Person, 'firstName'> = ({ value }) =>
+        value === 'other' ? error : undefined
+
+      form = injectForm<Person>({
+        defaultValues: {
+          firstName: 'FirstName',
+          lastName: 'LastName',
+        },
+      })
+    }
+
+    const { getByTestId, queryByText } = await render(TestComponent)
+    const input = getByTestId('fieldinput')
+    await user.type(input, 'other')
+    expect(queryByText(error)).not.toBeInTheDocument()
+  })
+
+  it('should validate on change if isTouched is true', async () => {
+    type Person = {
+      firstName: string
+      lastName: string
+    }
+    const error = 'Please enter a different value'
+
+    @Component({
+      selector: 'test-component',
+      standalone: true,
+      template: `
+        <ng-template
+          [tanstackField]="form"
+          name="firstName"
+          [defaultMeta]="{ isTouched: true }"
+          [validators]="{ onChange: otherValidator }"
+          let-field
+        >
+          <input
+            data-testid="fieldinput"
+            [value]="field.state.value"
+            (blur)="field.handleBlur()"
+            (input)="field.handleChange($any($event).target.value)"
+          />
+          <p>{{ field.getMeta().errorMap?.onChange }}</p>
+        </ng-template>
+      `,
+      imports: [TanStackField],
+    })
+    class TestComponent {
+      otherValidator: FieldValidateFn<Person, 'firstName'> = ({ value }) =>
+        value === 'other' ? error : undefined
+
+      form = injectForm<Person>({
+        defaultValues: {
+          firstName: '',
+          lastName: '',
+        },
+      })
+    }
+
+    const { getByTestId, queryByText, getByText } = await render(TestComponent)
+
+    const input = getByTestId('fieldinput')
+    expect(queryByText(error)).not.toBeInTheDocument()
+    await user.type(input, 'other')
+    expect(getByText(error)).toBeInTheDocument()
   })
 })
