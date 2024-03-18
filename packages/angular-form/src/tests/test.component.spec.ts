6925f596095e5d1e7044680eb678892b81f9a36b
Corbin Crutchley <git@crutchcorn.dev>
2024-03-18 13:20:44 -0600

    test: add tests for Angular adapter default values

--- packages/angular-form/src/tests/test.component.spec.ts
+++ packages/angular-form/src/tests/test.component.spec.ts
@@ -1,17 +1,79 @@
-import { render, screen } from '@testing-library/angular'
+import { render } from '@testing-library/angular'
 import { Component } from '@angular/core'
+import { describe, expect, it } from 'vitest'
+import { injectForm } from '../inject-form'
+import { TanStackField } from '../tanstack-field.directive'
 
-@Component({
-  selector: 'test-component',
-  standalone: true,
-  template: ` <p>Testing</p> `,
-})
-class TestComponent {}
+describe('TanStackFieldDirective', () => {
+  it('should allow to set default value', async () => {
+    @Component({
+      selector: 'test-component',
+      standalone: true,
+      template: `
+        <ng-template [tanstackField]="form" name="firstName" let-field>
+          <input
+            data-testid="fieldinput"
+            [value]="field.state.value"
+            (blur)="field.handleBlur()"
+            (input)="field.handleChange($any($event).target.value)"
+          />
+        </ng-template>
+      `,
+      imports: [TanStackField],
+    })
+    class TestComponent {
+      form = injectForm({
+        defaultValues: {
+          firstName: 'FirstName',
+          lastName: 'LastName',
+        },
+      })
+    }
 
-describe('TestComponent', () => {
-  test('should show message', async () => {
-    await render(TestComponent)
+    const { getByTestId } = await render(TestComponent)
 
-    expect(screen.getByText('Testing')).toBeInTheDocument()
+    const input = getByTestId('fieldinput')
+    expect(input).toHaveValue('FirstName')
+  })
+
+  it('should use field default value first', async () => {
+    type Person = {
+      firstName: string
+      lastName: string
+    }
+
+    @Component({
+      selector: 'test-component',
+      standalone: true,
+      template: `
+        <ng-template
+          [tanstackField]="form"
+          name="firstName"
+          defaultValue="otherName"
+          let-field
+        >
+          <input
+            data-testid="fieldinput"
+            [value]="field.state.value"
+            (blur)="field.handleBlur()"
+            (input)="field.handleChange($any($event).target.value)"
+          />
+        </ng-template>
+      `,
+      imports: [TanStackField],
+    })
+    class TestComponent {
+      form = injectForm<Person>({
+        defaultValues: {
+          firstName: 'FirstName',
+          lastName: 'LastName',
+        },
+      })
+    }
+
+    const { getByTestId } = await render(TestComponent)
+
+    const input = getByTestId('fieldinput')
+    expect(input).toHaveValue('otherName')
   })
 })
