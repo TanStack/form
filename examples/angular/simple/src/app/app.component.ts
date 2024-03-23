82c74fa87b5a1453f4942a772f4d315525b14969
Corbin Crutchley <git@crutchcorn.dev>
2024-03-22 18:53:38 -0600

    chore: improve angular example

--- examples/angular/simple/src/app/app.component.ts
+++ examples/angular/simple/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 import {
+  FieldValidateAsyncFn,
   FieldValidateFn,
   injectForm,
   TanStackField,
@@ -15,35 +16,64 @@ import {
       <ng-container
         [tanstackField]="form"
         name="firstName"
-        [validators]="{ onChange: required }"
-        #f="field"
+        [validators]="{
+          onChange: firstNameValidator,
+          onChangeAsyncDebounceMs: 500,
+          onChangeAsync: firstNameAsyncValidator
+        }"
+        #firstName="field"
       >
-        <label>
-          <div>First name:</div>
-          <input
-            [value]="f.api.state.value"
-            (blur)="f.api.handleBlur()"
-            (input)="f.api.handleChange($any($event).target.value)"
-          />
-        </label>
-        @for (error of f.api.state.meta.errors; track $index) {
+        <label [for]="firstName.api.name">First Name:</label>
+        <input
+          [id]="firstName.api.name"
+          [name]="firstName.api.name"
+          [value]="firstName.api.state.value"
+          (blur)="firstName.api.handleBlur()"
+          (input)="firstName.api.handleChange($any($event).target.value)"
+        />
+        @for (error of firstName.api.state.meta.touchedErrors; track $index) {
           <div style="color: red">
             {{ error }}
           </div>
         }
+        @if (firstName.api.state.meta.isValidating) {
+          <p>Validating...</p>
+        }
+      </ng-container>
+      <ng-container [tanstackField]="form" name="lastName" #lastName="field">
+        <label [for]="lastName.api.name">Last Name:</label>
+        <input
+          [id]="lastName.api.name"
+          [name]="lastName.api.name"
+          [value]="lastName.api.state.value"
+          (blur)="lastName.api.handleBlur()"
+          (input)="lastName.api.handleChange($any($event).target.value)"
+        />
       </ng-container>
       <button>Submit</button>
     </form>
   `,
 })
 export class AppComponent {
-  required: FieldValidateFn<any, string> = ({ value }) => {
-    return !value ? 'Required' : undefined
-  }
+  firstNameValidator: FieldValidateFn<any, any, any, any, string> = ({
+    value,
+  }) =>
+    !value
+      ? 'A first name is required'
+      : value.length < 3
+        ? 'First name must be at least 3 characters'
+        : undefined
+
+  firstNameAsyncValidator: FieldValidateAsyncFn<any, any, any, any, string> =
+    async ({ value }) => {
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+      return value.includes('error') && 'No "error" allowed in first name'
+    }
+
   form = injectForm({
     defaultValues: {
-      firstName: 'Ryan',
-      age: 25,
+      firstName: '',
+      lastName: '',
     },
     onSubmit({ value }: any) {
       console.log({ value })
