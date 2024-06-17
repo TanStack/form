e697c878fea3b85fae8eb273aa3ee7337f0ea2fd
Toma Rosa <toma.rosa@ik.me>
2024-06-17 21:22:04 +0200

    test: add yup validator transformErrors base test

--- packages/yup-form-adapter/src/tests/FieldApi.spec.ts
+++ packages/yup-form-adapter/src/tests/FieldApi.spec.ts
@@ -117,4 +117,36 @@ describe('yup field api', () => {
     await sleep(10)
     expect(field.getMeta().errors).toEqual([])
   })
+
+  it('should transform errors to display only the first error message', () => {
+    const form = new FormApi({
+      defaultValues: {
+        name: '',
+      },
+    })
+
+    const field = new FieldApi({
+      form,
+      validatorAdapter: yupValidator({
+        transformErrors: (errors) => errors[0],
+      }),
+      name: 'name',
+      validators: {
+        onChange: yup
+          .string()
+          .min(3, 'You must have a length of at least 3')
+          .uuid('UUID'),
+      },
+    })
+
+    field.mount()
+
+    expect(field.getMeta().errors).toEqual([])
+    field.setValue('aa', { touch: true })
+    expect(field.getMeta().errors).toEqual([
+      'You must have a length of at least 3',
+    ])
+    field.setValue('aaa', { touch: true })
+    expect(field.getMeta().errors).toEqual(['UUID'])
+  })
 })
