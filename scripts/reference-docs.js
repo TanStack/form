5aaf73c63cd794485f5bed1e917a8daa05a297dc
Corbin Crutchley <git@crutchcorn.dev>
2024-06-19 19:05:49 -0700

    chore: migrate reference docs to take multiple paths

--- scripts/reference-docs.js
+++ scripts/reference-docs.js
@@ -5,30 +5,43 @@ import 'typedoc-plugin-markdown'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
+const options = {
+  plugin: [
+    'typedoc-plugin-markdown',
+    path.resolve(__dirname, './typedoc-remove-prefix.mjs'),
+  ],
+  hideGenerator: true,
+  readme: 'none',
+  flattenOutputFiles: true,
+  entryFileName: 'index',
+  hideBreadcrumbs: true,
+  hidePageHeader: true,
+  useCodeBlocks: true,
+  excludePrivate: true,
+}
+
+const packages = [
+  {
+    name: 'form-core',
+    entryPoint: path.resolve(__dirname, '../packages/form-core/src/index.ts'),
+    tsconfig: path.resolve(__dirname, '../packages/form-core/tsconfig.json'),
+    outputDir: path.resolve(__dirname, '../docs/reference'),
+  },
+]
+
 async function main() {
-  const base = path.resolve(__dirname, '../packages/form-core')
-  const app = await TypeDoc.Application.bootstrapWithPlugins({
-    entryPoints: [path.resolve(base, 'src/index.ts')],
-    plugin: [
-      'typedoc-plugin-markdown',
-      path.resolve(__dirname, './typedoc-remove-prefix.mjs'),
-    ],
-    tsconfig: path.resolve(base, 'tsconfig.json'),
-    hideGenerator: true,
-    readme: 'none',
-    flattenOutputFiles: true,
-    entryFileName: 'index',
-    hideBreadcrumbs: true,
-    hidePageHeader: true,
-    useCodeBlocks: true,
-    excludePrivate: true,
-  })
+  for (const pkg of packages) {
+    const app = await TypeDoc.Application.bootstrapWithPlugins({
+      ...options,
+      entryPoints: [pkg.entryPoint],
+      tsconfig: pkg.tsconfig,
+    })
 
-  const project = await app.convert()
+    const project = await app.convert()
 
-  if (project) {
-    const outputDir = 'docs/reference'
-    await app.generateDocs(project, outputDir)
+    if (project) {
+      await app.generateDocs(project, pkg.outputDir)
+    }
   }
 }
 
