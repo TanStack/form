a6313b7699753752ae30ff16c169e0b08c2369e8
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-07-19 19:25:28 +1000

    ci: Add autofix.yml (#870)

--- scripts/config.js
+++ scripts/config.js
@@ -54,13 +54,10 @@ export const branchConfigs = {
   main: {
     prerelease: false,
   },
-  next: {
+  alpha: {
     prerelease: true,
   },
   beta: {
-    prerelease: true,
-  },
-  alpha: {
     prerelease: true,
   },
 }
