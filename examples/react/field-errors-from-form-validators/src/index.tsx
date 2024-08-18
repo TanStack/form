acb374e4c0ed8b44539ac57fbe46c923ecce2085
Fülöp Kovács <kovacs.fulop@gmail.com>
2024-08-18 23:25:36 +0200

    Update the field-errors-from-form-validators examples

--- examples/react/field-errors-from-form-validators/src/index.tsx
+++ examples/react/field-errors-from-form-validators/src/index.tsx
@@ -1,26 +1,40 @@
+import { useForm } from '@tanstack/react-form'
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
-import { useForm } from '@tanstack/react-form'
 
 async function verifyAgeOnServer(age: number) {
   return age >= 13
 }
 
+async function checkIfUsernameIsTaken(name: string) {
+  const usernames = ['user-1', 'user-2', 'user-3']
+  return usernames.includes(name)
+}
+
 export default function App() {
   const form = useForm({
     defaultValues: {
+      username: '',
       age: 0,
     },
     validators: {
       onSubmitAsync: async ({ value }) => {
         console.log({ value })
         // Verify the age on the server
-        const isOlderThan13 = await verifyAgeOnServer(value.age)
-        if (!isOlderThan13) {
+        const isNotOlderThan13 = !(await verifyAgeOnServer(value.age))
+
+        // Verify the availability of the username on the server
+        const isUsernameTaken = await checkIfUsernameIsTaken(value.username)
+
+        if (isNotOlderThan13 || isUsernameTaken) {
           return {
-            form: 'Invalid data', // The `form` key is optional
+            // The `form` key is optional
+            form: 'Invalid data',
             fields: {
-              age: 'Must be 13 or older to sign',
+              ...(isUsernameTaken ? { username: 'Username is taken' } : {}),
+              ...(isNotOlderThan13
+                ? { age: 'Must be 13 or older to sign' }
+                : {}),
             },
           }
         }
@@ -40,9 +54,36 @@ export default function App() {
           void form.handleSubmit()
         }}
       >
-        <form.Field name="age">
-          {(field) => (
-            <>
+        <form.Field
+          name="username"
+          validators={{
+            onSubmit: ({ value }) => (!value ? 'Required field' : null),
+          }}
+          children={(field) => (
+            <div>
+              <label htmlFor={field.name}>Username:</label>
+              <input
+                id={field.name}
+                name={field.name}
+                value={field.state.value}
+                onChange={(e) => {
+                  field.handleChange(e.target.value)
+                }}
+              />
+              {field.state.meta.errors.length > 0 ? (
+                <em role="alert">{field.state.meta.errors.join(', ')}</em>
+              ) : null}
+            </div>
+          )}
+        />
+
+        <form.Field
+          name="age"
+          validators={{
+            onSubmit: ({ value }) => (!value ? 'Required field' : null),
+          }}
+          children={(field) => (
+            <div>
               <label htmlFor={field.name}>Age:</label>
               <input
                 id={field.name}
@@ -54,9 +95,10 @@ export default function App() {
               {field.state.meta.errors.length > 0 ? (
                 <em role="alert">{field.state.meta.errors.join(', ')}</em>
               ) : null}
-            </>
+            </div>
           )}
-        </form.Field>
+        />
+
         {form.state.errorMap.onSubmit ? (
           <div>
             <em>
