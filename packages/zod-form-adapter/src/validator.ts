d23e697df7a811cb695711b8c2cb2ff62c5ba2e8
Leonardo Montini <lion.48m@gmail.com>
2024-09-14 16:17:37 +0200

    feat: apply transformError to all fields

--- packages/zod-form-adapter/src/validator.ts
+++ packages/zod-form-adapter/src/validator.ts
@@ -1,53 +1,69 @@
 import { setBy } from '@tanstack/form-core'
-import type { Validator, ValidatorAdapterParams } from '@tanstack/form-core'
+import type {
+  ValidationError,
+  Validator,
+  ValidatorAdapterParams,
+} from '@tanstack/form-core'
 import type { ZodIssue, ZodType } from 'zod'
 
 type Params = ValidatorAdapterParams<ZodIssue>
+type TransformFn = NonNullable<Params['transformErrors']>
 
-export function prefixSchemaToErrors(zodErrors: ZodIssue[]) {
-  let schema = {} as object
+export function prefixSchemaToErrors(
+  zodErrors: ZodIssue[],
+  transformErrors: TransformFn,
+) {
+  let schema = {} as Record<string, ZodIssue[]>
   for (const zodError of zodErrors) {
-    schema = setBy(schema, zodError.path, () => zodError.message)
+    schema = setBy(schema, zodError.path, (errors: ZodIssue[] | undefined) =>
+      (errors ?? []).concat(zodError),
+    )
   }
-  return schema
+
+  const transformedSchema = {} as Record<string, ValidationError>
+
+  for (const key in schema) {
+    transformedSchema[key] = transformErrors(schema[key] ?? [])
+  }
+
+  return transformedSchema
 }
 
-export function defaultFormTransformer(zodErrors: ZodIssue[]) {
-  return {
-    form: mapIssuesToSingleString(zodErrors),
-    fields: prefixSchemaToErrors(zodErrors),
-  }
+export function defaultFormTransformer(transformErrors: TransformFn) {
+  return (zodErrors: ZodIssue[]) => ({
+    form: transformErrors(zodErrors),
+    fields: prefixSchemaToErrors(zodErrors, transformErrors),
+  })
 }
-
-export const mapIssuesToSingleString = (issues: ZodIssue[]) =>
-  issues.map((issue) => issue.message).join(', ')
 
 export const zodValidator =
   (params: Params = {}): Validator<unknown, ZodType> =>
   () => {
+    const transformFieldErrors =
+      params.transformErrors ??
+      ((issues: ZodIssue[]) => issues.map((issue) => issue.message).join(', '))
+
+    const getTransformStrategy = (validationSource: 'form' | 'field') =>
+      validationSource === 'form'
+        ? defaultFormTransformer(transformFieldErrors)
+        : transformFieldErrors
+
     return {
       validate({ value, validationSource }, fn) {
         const result = fn.safeParse(value)
         if (result.success) return
-        const transformErrors =
-          params.transformErrors ??
-          (validationSource === 'form'
-            ? defaultFormTransformer
-            : mapIssuesToSingleString)
 
-        return transformErrors(result.error.issues)
+        const transformer = getTransformStrategy(validationSource)
+
+        return transformer(result.error.issues)
       },
       async validateAsync({ value, validationSource }, fn) {
         const result = await fn.safeParseAsync(value)
         if (result.success) return
 
-        const transformErrors =
-          params.transformErrors ??
-          (validationSource === 'form'
-            ? defaultFormTransformer
-            : mapIssuesToSingleString)
+        const transformer = getTransformStrategy(validationSource)
 
-        return transformErrors(result.error.issues)
+        return transformer(result.error.issues)
       },
     }
   }
--- packages/zod-form-adapter/tests/FormApi.spec.ts
+++ packages/zod-form-adapter/tests/FormApi.spec.ts
@@ -56,4 +56,85 @@ describe('zod form api', () => {
     field.setValue('asdf')
     expect(field.getMeta().errors).toEqual([])
   })
+
+  it("should set field errors with the form validator's onChange", () => {
+    const form = new FormApi({
+      defaultValues: {
+        name: '',
+        surname: '',
+      },
+      validatorAdapter: zodValidator(),
+      validators: {
+        onChange: z.object({
+          name: z
+            .string()
+            .min(3, 'You must have a length of at least 3')
+            .endsWith('a', 'You must end with an "a"'),
+          surname: z.string().min(3, 'You must have a length of at least 3'),
+        }),
+      },
+    })
+
+    const nameField = new FieldApi({
+      form,
+      name: 'name',
+    })
+
+    const surnameField = new FieldApi({
+      form,
+      name: 'surname',
+    })
+
+    nameField.mount()
+    surnameField.mount()
+
+    expect(nameField.getMeta().errors).toEqual([])
+    nameField.setValue('q')
+    expect(nameField.getMeta().errors).toEqual([
+      'You must have a length of at least 3, You must end with an "a"',
+    ])
+    expect(surnameField.getMeta().errors).toEqual([
+      'You must have a length of at least 3',
+    ])
+    nameField.setValue('qwer')
+    expect(nameField.getMeta().errors).toEqual(['You must end with an "a"'])
+    nameField.setValue('qwera')
+    expect(nameField.getMeta().errors).toEqual([])
+  })
+
+  it("should set field errors with the form validator's onChange and transform them", () => {
+    const form = new FormApi({
+      defaultValues: {
+        name: '',
+      },
+      validatorAdapter: zodValidator({
+        transformErrors: (errors) => errors[0]?.message,
+      }),
+      validators: {
+        onChange: z.object({
+          name: z
+            .string()
+            .min(3, 'You must have a length of at least 3')
+            .endsWith('a', 'You must end with an "a"'),
+        }),
+      },
+    })
+
+    const field = new FieldApi({
+      form,
+      name: 'name',
+    })
+
+    field.mount()
+
+    expect(field.getMeta().errors).toEqual([])
+    field.setValue('q')
+    expect(field.getMeta().errors).toEqual([
+      'You must have a length of at least 3',
+    ])
+    field.setValue('qwer')
+    expect(field.getMeta().errors).toEqual(['You must end with an "a"'])
+    field.setValue('qwera')
+    expect(field.getMeta().errors).toEqual([])
+  })
 })
