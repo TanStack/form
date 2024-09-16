198c08a2937744e2fbbe944a06e459065a5437b4
Leonardo Montini <lion.48m@gmail.com>
2024-09-16 21:44:57 +0200

    feat: spread valibot errors into fields, nested and arrays

--- packages/valibot-form-adapter/src/validator.ts
+++ packages/valibot-form-adapter/src/validator.ts
@@ -1,38 +1,70 @@
 import { getDotPath, safeParse, safeParseAsync } from 'valibot'
 import { setBy } from '@tanstack/form-core'
-import type { Validator, ValidatorAdapterParams } from '@tanstack/form-core'
+import type {
+  ValidationError,
+  Validator,
+  ValidatorAdapterParams,
+} from '@tanstack/form-core'
 import type {
   BaseIssue,
   GenericIssue,
   GenericSchema,
   GenericSchemaAsync,
+  ValiError,
 } from 'valibot'
 
 type Params = ValidatorAdapterParams<GenericIssue>
+type TransformFn = NonNullable<Params['transformErrors']>
 
-export function prefixSchemaToErrors(errors: Array<BaseIssue<unknown>>) {
-  let schema = {} as object
-  for (const valibotError of errors) {
-    schema = setBy(schema, getDotPath(valibotError), () => valibotError.message)
+export function prefixSchemaToErrors(
+  valiErrors: GenericIssue[],
+  transformErrors: TransformFn,
+) {
+  const schema = new Map<string, GenericIssue[]>()
+
+  for (const valiError of valiErrors) {
+    if (!valiError.path) continue
+
+    const path = valiError.path
+      .map(({ key: segment }) =>
+        typeof segment === 'number' ? `[${segment}]` : segment,
+      )
+      .join('.')
+      .replace(/\.\[/g, '[')
+    schema.set(path, (schema.get(path) ?? []).concat(valiError))
   }
-  return schema
+
+  const transformedSchema = {} as Record<string, ValidationError>
+
+  schema.forEach((value, key) => {
+    transformedSchema[key] = transformErrors(value)
+  })
+
+  return transformedSchema
 }
 
-export function defaultFormTransformer(errors: Array<BaseIssue<unknown>>) {
-  return {
-    form: mapIssuesToSingleString(errors),
-    fields: prefixSchemaToErrors(errors),
-  }
+export function defaultFormTransformer(transformErrors: TransformFn) {
+  return (zodErrors: GenericIssue[]) => ({
+    form: transformErrors(zodErrors),
+    fields: prefixSchemaToErrors(zodErrors, transformErrors),
+  })
 }
-
-export const mapIssuesToSingleString = (errors: Array<BaseIssue<unknown>>) =>
-  errors.map((error) => error.message).join(', ')
 
 export const valibotValidator =
   (
     params: Params = {},
   ): Validator<unknown, GenericSchema | GenericSchemaAsync> =>
   () => {
+    const transformFieldErrors =
+      params.transformErrors ??
+      ((issues: GenericIssue[]) =>
+        issues.map((issue) => issue.message).join(', '))
+
+    const getTransformStrategy = (validationSource: 'form' | 'field') =>
+      validationSource === 'form'
+        ? defaultFormTransformer(transformFieldErrors)
+        : transformFieldErrors
+
     return {
       validate({ value, validationSource }, fn) {
         if (fn.async) return
@@ -40,24 +72,20 @@ export const valibotValidator =
           abortPipeEarly: false,
         })
         if (result.success) return
-        const transformErrors = params.transformErrors
-          ? params.transformErrors
-          : validationSource === 'form'
-            ? defaultFormTransformer
-            : mapIssuesToSingleString
-        return transformErrors(result.issues)
+
+        const transformer = getTransformStrategy(validationSource)
+
+        return transformer(result.issues)
       },
       async validateAsync({ value, validationSource }, fn) {
         const result = await safeParseAsync(fn, value, {
           abortPipeEarly: false,
         })
         if (result.success) return
-        const transformErrors = params.transformErrors
-          ? params.transformErrors
-          : validationSource === 'form'
-            ? defaultFormTransformer
-            : mapIssuesToSingleString
-        return transformErrors(result.issues)
+
+        const transformer = getTransformStrategy(validationSource)
+
+        return transformer(result.issues)
       },
     }
   }
