ab4c57c9828bf3af58b2f957c5d20ab7329c5b15
Leonardo Montini <lion.48m@gmail.com>
2024-11-19 20:47:17 +0100

    tests: add test for asyncAlways in the field

--- packages/form-core/tests/FormApi.spec.ts
+++ packages/form-core/tests/FormApi.spec.ts
@@ -1951,6 +1951,40 @@ describe('form api', () => {
     expect(formAsyncChange).not.toHaveBeenCalled()
   })
 
+  it('runs form async validator if field sync has errored and asyncAlways is true', async () => {
+    const formAsyncChange = vi.fn()
+    const formSyncChange = vi.fn()
+
+    const form = new FormApi({
+      defaultValues: {
+        firstName: '',
+      },
+      validators: {
+        onChange: formSyncChange,
+        onChangeAsync: formAsyncChange,
+      },
+    })
+
+    const firstNameField = new FieldApi({
+      form,
+      name: 'firstName',
+      asyncAlways: true,
+      validators: {
+        onChange: ({ value }) => (value.length > 0 ? undefined : 'field error'),
+      },
+    })
+
+    form.mount()
+    firstNameField.mount()
+
+    firstNameField.handleChange('')
+    await vi.runAllTimersAsync()
+
+    expect(formSyncChange).toHaveBeenCalled()
+    expect(firstNameField.state.meta.errorMap.onChange).toBe('field error')
+    expect(formAsyncChange).toHaveBeenCalled()
+  })
+
   it("should set errors for the fields from the form's onChange validator", async () => {
     const form = new FormApi({
       defaultValues: {
