65c31b69f1418b9f2123571e3f46dce8cec1076a
Corbin Crutchley <git@crutchcorn.dev>
2025-01-09 02:17:54 -0800

    chore: fix build of Start adapter

--- packages/react-form/src/start/createServerValidate.tsx
+++ packages/react-form/src/start/createServerValidate.tsx
@@ -1,40 +1,36 @@
 import { decode } from 'decode-formdata'
+import { isFormValidationError } from '@tanstack/form-core'
 import { _tanstackInternalsCookie } from './utils'
 import { ServerValidateError } from './error'
-import type {
-  FormOptions,
-  FormValidationError,
-  ValidationError,
-  Validator,
-} from '@tanstack/form-core'
+import type { FormOptions, Validator } from '@tanstack/form-core'
 import type { FetchFn } from '@tanstack/start'
 import type { ServerFormState } from './types'
 
 type Ctx = Parameters<FetchFn<FormData, unknown>>[1]
 
-type OnServerValidateFn<TFormData> = (props: {
+type OnServerValidateFn<TFormData, TOnServerReturn = undefined> = (props: {
   value: TFormData
-}) => ValidationError | Promise<ValidationError>
+}) => TOnServerReturn
 
 type OnServerValidateOrFn<
   TFormData,
   TFormValidator extends Validator<TFormData, unknown> | undefined = undefined,
+  TOnServerReturn = undefined,
 > =
-  TFormValidator extends Validator<TFormData, infer FFN>
-    ? FFN | OnServerValidateFn<TFormData>
-    : OnServerValidateFn<TFormData>
+  TFormValidator extends Validator<TFormData, infer FFN, infer _TOnServerReturn>
+    ? FFN | OnServerValidateFn<TFormData, TOnServerReturn>
+    : OnServerValidateFn<TFormData, TOnServerReturn>
 
 interface CreateServerValidateOptions<
   TFormData,
   TFormValidator extends Validator<TFormData, unknown> | undefined = undefined,
+  TOnServerReturn = undefined,
 > extends FormOptions<TFormData, TFormValidator> {
-  onServerValidate: OnServerValidateOrFn<TFormData, TFormValidator>
-}
-
-const isFormValidationError = (
-  error: unknown,
-): error is FormValidationError<unknown> => {
-  return typeof error === 'object'
+  onServerValidate: OnServerValidateOrFn<
+    TFormData,
+    TFormValidator,
+    TOnServerReturn
+  >
 }
 
 export const createServerValidate =
@@ -43,8 +39,13 @@ export const createServerValidate =
     TFormValidator extends
       | Validator<TFormData, unknown>
       | undefined = undefined,
+    TOnServerReturn = undefined,
   >(
-    defaultOpts: CreateServerValidateOptions<TFormData, TFormValidator>,
+    defaultOpts: CreateServerValidateOptions<
+      TFormData,
+      TFormValidator,
+      TOnServerReturn
+    >,
   ) =>
   async (ctx: Ctx, formData: FormData, info?: Parameters<typeof decode>[1]) => {
     const { validatorAdapter, onServerValidate } = defaultOpts
@@ -64,26 +65,23 @@ export const createServerValidate =
 
     const data = decode(formData, info) as never as TFormData
 
-    const onServerError = await runValidator({
+    const onServerError = (await runValidator({
       value: data,
       validationSource: 'form',
-    })
+    })) as TOnServerReturn | undefined
 
     if (!onServerError) return
 
-    const onServerErrorStr =
-      onServerError &&
-      typeof onServerError !== 'string' &&
-      isFormValidationError(onServerError)
-        ? onServerError.form
-        : onServerError
+    const onServerErrorVal = (
+      isFormValidationError(onServerError) ? onServerError.form : onServerError
+    ) as TOnServerReturn
 
-    const formState: ServerFormState<TFormData> = {
+    const formState: ServerFormState<TFormData, TOnServerReturn> = {
       errorMap: {
         onServer: onServerError,
       },
       values: data,
-      errors: onServerErrorStr ? [onServerErrorStr] : [],
+      errors: onServerErrorVal ? [onServerErrorVal] : [],
     }
 
     const cookie = await _tanstackInternalsCookie.serialize(formState)
--- packages/react-form/src/start/types.ts
+++ packages/react-form/src/start/types.ts
@@ -1,6 +1,16 @@
 import type { FormState } from '@tanstack/form-core'
 
-export type ServerFormState<TFormData> = Pick<
-  FormState<TFormData>,
+export type ServerFormState<TFormData, TOnServerReturn = undefined> = Pick<
+  FormState<
+    TFormData,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    TOnServerReturn
+  >,
   'values' | 'errors' | 'errorMap'
 >
