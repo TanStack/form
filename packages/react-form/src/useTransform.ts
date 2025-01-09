00963bda077e6e6771799b70e60c80623625e6e3
Corbin Crutchley <git@crutchcorn.dev>
2025-01-09 01:22:42 -0800

    chore: move useTransform hook to new types

--- packages/react-form/src/useTransform.ts
+++ packages/react-form/src/useTransform.ts
@@ -3,10 +3,39 @@ import type { FormApi, FormTransform, Validator } from '@tanstack/form-core'
 export function useTransform<
   TFormData,
   TFormValidator extends Validator<TFormData, unknown> | undefined = undefined,
+  TOnMountReturn = undefined,
+  TOnChangeReturn = undefined,
+  TOnChangeAsyncReturn = undefined,
+  TOnBlurReturn = undefined,
+  TOnBlurAsyncReturn = undefined,
+  TOnSubmitReturn = undefined,
+  TOnSubmitAsyncReturn = undefined,
 >(
-  fn: (formBase: FormApi<any, any>) => FormApi<TFormData, TFormValidator>,
+  fn: (
+    formBase: FormApi<any, any, any, any, any, any, any, any, any>,
+  ) => FormApi<
+    TFormData,
+    TFormValidator,
+    TOnMountReturn,
+    TOnChangeReturn,
+    TOnChangeAsyncReturn,
+    TOnBlurReturn,
+    TOnBlurAsyncReturn,
+    TOnSubmitReturn,
+    TOnSubmitAsyncReturn
+  >,
   deps: unknown[],
-): FormTransform<TFormData, TFormValidator> {
+): FormTransform<
+  TFormData,
+  TFormValidator,
+  TOnMountReturn,
+  TOnChangeReturn,
+  TOnChangeAsyncReturn,
+  TOnBlurReturn,
+  TOnBlurAsyncReturn,
+  TOnSubmitReturn,
+  TOnSubmitAsyncReturn
+> {
   return {
     fn,
     deps,
