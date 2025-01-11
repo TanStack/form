56007970cc0eaca27d909575fbc3e1e9d08339fc
Corbin Crutchley <git@crutchcorn.dev>
2025-01-11 01:30:20 -0800

    chore: fix Vue examples

--- examples/vue/yup/src/App.vue
+++ examples/vue/yup/src/App.vue
@@ -36,9 +36,6 @@ const onChangeFirstName = yup
     "
   >
     <div>
-      <!-- Ignore errors in form.Field temporary due to a bug in Vue:-->
-      <!-- https://github.com/vuejs/language-tools/issues/3782 -->
-      <!-- @vue-ignore -->
       <form.Field
         name="firstName"
         :validators="{
