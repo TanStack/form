8f7d448b130e0ee88c8c21bc752c58640812bff0
Corbin Crutchley <git@crutchcorn.dev>
2025-01-14 17:01:42 -0800

    fix: meta should not regenerate every render

--- packages/form-core/src/FormApi.ts
+++ packages/form-core/src/FormApi.ts
@@ -416,8 +416,10 @@ export class FormApi<
             fieldName as never
           ] as FieldMetaBase | undefined
 
-          let fieldErrors =
-            prevVal?.[fieldName as never as keyof typeof prevVal]?.errors
+          const prevFieldInfo =
+            prevVal?.[fieldName as never as keyof typeof prevVal]
+
+          let fieldErrors = prevFieldInfo?.errors
           if (!prevBaseVal || currBaseVal.errorMap !== prevBaseVal.errorMap) {
             // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
             fieldErrors = Object.values(currBaseVal.errorMap ?? {}).filter(
@@ -427,6 +429,15 @@ export class FormApi<
 
           // As a primitive, we don't need to aggressively persist the same referencial value for performance reasons
           const isFieldPristine = !currBaseVal.isDirty
+
+          if (
+            prevFieldInfo &&
+            prevFieldInfo.isPristine === isFieldPristine &&
+            prevFieldInfo.errors === fieldErrors
+          ) {
+            fieldMeta[fieldName] = prevFieldInfo
+            continue
+          }
 
           fieldMeta[fieldName] = {
             ...currBaseVal,
