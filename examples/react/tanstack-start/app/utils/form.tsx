d5e06c7b19b433ba032b444f9a195fb991d0d556
Corbin Crutchley <git@crutchcorn.dev>
2025-03-01 00:06:55 -0800

    docs: fix tanstack start demo (#1196)
    * docs: fix tanstack start demo
    
    * ci: apply automated fixes and generate docs
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- examples/react/tanstack-start/app/utils/form.tsx
+++ examples/react/tanstack-start/app/utils/form.tsx
@@ -1,18 +1,11 @@
 import { createServerFn } from '@tanstack/react-start'
-import { formOptions } from '@tanstack/react-form'
 import {
   ServerValidateError,
   createServerValidate,
   getFormData,
 } from '@tanstack/react-form/start'
 import { setResponseStatus } from '@tanstack/react-start/server'
-
-export const formOpts = formOptions({
-  defaultValues: {
-    firstName: '',
-    age: 0,
-  },
-})
+import { formOpts } from './form-isomorphic'
 
 const serverValidate = createServerValidate({
   ...formOpts,
