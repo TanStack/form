5dbab3aa7162e0540264a1a13b2fba754ae5b529
chorobin <chrishorobin@hotmail.com>
2025-03-10 23:56:46 +0100

    perf(vue-form): add interface changes to vue

--- packages/solid-form/src/createField.tsx
+++ packages/solid-form/src/createField.tsx
@@ -427,8 +427,55 @@ export type FieldComponent<
 >({
   children,
   ...fieldOptions
-}: 
-  FieldComponentBoundProps<
+}: FieldComponentBoundProps<
+  TParentData,
+  TName,
+  TData,
+  TOnMount,
+  TOnChange,
+  TOnChangeAsync,
+  TOnBlur,
+  TOnBlurAsync,
+  TOnSubmit,
+  TOnSubmitAsync,
+  TFormOnMount,
+  TFormOnChange,
+  TFormOnChangeAsync,
+  TFormOnBlur,
+  TFormOnBlurAsync,
+  TFormOnSubmit,
+  TFormOnSubmitAsync,
+  TFormOnServer,
+  TParentSubmitMeta
+>) => JSXElement
+
+interface FieldComponentProps<
+  TParentData,
+  TName extends DeepKeys<TParentData>,
+  TData extends DeepValue<TParentData, TName>,
+  TOnMount extends undefined | FieldValidateOrFn<TParentData, TName, TData>,
+  TOnChange extends undefined | FieldValidateOrFn<TParentData, TName, TData>,
+  TOnChangeAsync extends
+    | undefined
+    | FieldAsyncValidateOrFn<TParentData, TName, TData>,
+  TOnBlur extends undefined | FieldValidateOrFn<TParentData, TName, TData>,
+  TOnBlurAsync extends
+    | undefined
+    | FieldAsyncValidateOrFn<TParentData, TName, TData>,
+  TOnSubmit extends undefined | FieldValidateOrFn<TParentData, TName, TData>,
+  TOnSubmitAsync extends
+    | undefined
+    | FieldAsyncValidateOrFn<TParentData, TName, TData>,
+  TFormOnMount extends undefined | FormValidateOrFn<TParentData>,
+  TFormOnChange extends undefined | FormValidateOrFn<TParentData>,
+  TFormOnChangeAsync extends undefined | FormAsyncValidateOrFn<TParentData>,
+  TFormOnBlur extends undefined | FormValidateOrFn<TParentData>,
+  TFormOnBlurAsync extends undefined | FormAsyncValidateOrFn<TParentData>,
+  TFormOnSubmit extends undefined | FormValidateOrFn<TParentData>,
+  TFormOnSubmitAsync extends undefined | FormAsyncValidateOrFn<TParentData>,
+  TFormOnServer extends undefined | FormAsyncValidateOrFn<TParentData>,
+  TParentSubmitMeta,
+> extends CreateFieldOptions<
     TParentData,
     TName,
     TData,
@@ -448,55 +495,7 @@ export type FieldComponent<
     TFormOnSubmitAsync,
     TFormOnServer,
     TParentSubmitMeta
->) => JSXElement
-
-interface FieldComponentProps<
-TParentData,
-TName extends DeepKeys<TParentData>,
-TData extends DeepValue<TParentData, TName>,
-TOnMount extends undefined | FieldValidateOrFn<TParentData, TName, TData>,
-TOnChange extends undefined | FieldValidateOrFn<TParentData, TName, TData>,
-TOnChangeAsync extends
-  | undefined
-  | FieldAsyncValidateOrFn<TParentData, TName, TData>,
-TOnBlur extends undefined | FieldValidateOrFn<TParentData, TName, TData>,
-TOnBlurAsync extends
-  | undefined
-  | FieldAsyncValidateOrFn<TParentData, TName, TData>,
-TOnSubmit extends undefined | FieldValidateOrFn<TParentData, TName, TData>,
-TOnSubmitAsync extends
-  | undefined
-  | FieldAsyncValidateOrFn<TParentData, TName, TData>,
-TFormOnMount extends undefined | FormValidateOrFn<TParentData>,
-TFormOnChange extends undefined | FormValidateOrFn<TParentData>,
-TFormOnChangeAsync extends undefined | FormAsyncValidateOrFn<TParentData>,
-TFormOnBlur extends undefined | FormValidateOrFn<TParentData>,
-TFormOnBlurAsync extends undefined | FormAsyncValidateOrFn<TParentData>,
-TFormOnSubmit extends undefined | FormValidateOrFn<TParentData>,
-TFormOnSubmitAsync extends undefined | FormAsyncValidateOrFn<TParentData>,
-TFormOnServer extends undefined | FormAsyncValidateOrFn<TParentData>,
-TParentSubmitMeta,
-> extends CreateFieldOptions<
-TParentData,
-TName,
-TData,
-TOnMount,
-TOnChange,
-TOnChangeAsync,
-TOnBlur,
-TOnBlurAsync,
-TOnSubmit,
-TOnSubmitAsync,
-TFormOnMount,
-TFormOnChange,
-TFormOnChangeAsync,
-TFormOnBlur,
-TFormOnBlurAsync,
-TFormOnSubmit,
-TFormOnSubmitAsync,
-TFormOnServer,
-TParentSubmitMeta
-> {
+  > {
   children: (
     fieldApi: () => FieldApi<
       TParentData,
