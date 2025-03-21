30b7cb3bf06943d581c6aae2048d2ea70679c032
Harry Whorlow <harry.whorlow@technoly.com>
2025-03-21 13:39:00 +0100

    chore: export formOptions

--- examples/react/tanstack-start/app/utils/form-isomorphic.ts
+++ examples/react/tanstack-start/app/utils/form-isomorphic.ts
@@ -1,4 +1,4 @@
-import { formOptions } from '@tanstack/react-form'
+import { formOptions } from '@tanstack/react-form/start'
 
 export const formOpts = formOptions({
   defaultValues: {
--- packages/react-form/src/start/index.ts
+++ packages/react-form/src/start/index.ts
@@ -1,3 +1,5 @@
+export * from '@tanstack/form-core'
+
 export * from './createServerValidate'
 export * from './getFormData'
 export * from './error'
