12e68ac114422082e97661ddae42bc76f6cb3db8
Corbin Crutchley <git@crutchcorn.dev>
2025-06-17 02:36:01 -0700

    chore: fixed issues with change detection

--- packages/angular-form/src/tanstack-field.directive.ts
+++ packages/angular-form/src/tanstack-field.directive.ts
@@ -1,12 +1,15 @@
 import {
+  ChangeDetectorRef,
   Directive,
-  OnChanges,
-  OnDestroy,
+  Injector,
   OnInit,
   booleanAttribute,
   computed,
+  effect,
+  inject,
   input,
   numberAttribute,
+  untracked,
 } from '@angular/core'
 import {
   FieldApi,
@@ -15,6 +18,7 @@ import {
   FieldValidateOrFn,
   FormApi,
 } from '@tanstack/form-core'
+import { injectStore } from '@tanstack/angular-store'
 import type {
   DeepKeys,
   DeepValue,
@@ -31,33 +35,32 @@ import type {
   exportAs: 'field',
 })
 export class TanStackField<
-    TParentData,
-    const TName extends DeepKeys<TParentData>,
-    TData extends DeepValue<TParentData, TName>,
-    TOnMount extends undefined | FieldValidateOrFn<TParentData, TName, TData>,
-    TOnChange extends undefined | FieldValidateOrFn<TParentData, TName, TData>,
-    TOnChangeAsync extends
-      | undefined
-      | FieldAsyncValidateOrFn<TParentData, TName, TData>,
-    TOnBlur extends undefined | FieldValidateOrFn<TParentData, TName, TData>,
-    TOnBlurAsync extends
-      | undefined
-      | FieldAsyncValidateOrFn<TParentData, TName, TData>,
-    TOnSubmit extends undefined | FieldValidateOrFn<TParentData, TName, TData>,
-    TOnSubmitAsync extends
-      | undefined
-      | FieldAsyncValidateOrFn<TParentData, TName, TData>,
-    TFormOnMount extends undefined | FormValidateOrFn<TParentData>,
-    TFormOnChange extends undefined | FormValidateOrFn<TParentData>,
-    TFormOnChangeAsync extends undefined | FormAsyncValidateOrFn<TParentData>,
-    TFormOnBlur extends undefined | FormValidateOrFn<TParentData>,
-    TFormOnBlurAsync extends undefined | FormAsyncValidateOrFn<TParentData>,
-    TFormOnSubmit extends undefined | FormValidateOrFn<TParentData>,
-    TFormOnSubmitAsync extends undefined | FormAsyncValidateOrFn<TParentData>,
-    TFormOnServer extends undefined | FormAsyncValidateOrFn<TParentData>,
-    TSubmitMeta,
-  >
-  implements OnInit, OnChanges, OnDestroy
+  TParentData,
+  const TName extends DeepKeys<TParentData>,
+  TData extends DeepValue<TParentData, TName>,
+  TOnMount extends undefined | FieldValidateOrFn<TParentData, TName, TData>,
+  TOnChange extends undefined | FieldValidateOrFn<TParentData, TName, TData>,
+  TOnChangeAsync extends
+    | undefined
+    | FieldAsyncValidateOrFn<TParentData, TName, TData>,
+  TOnBlur extends undefined | FieldValidateOrFn<TParentData, TName, TData>,
+  TOnBlurAsync extends
+    | undefined
+    | FieldAsyncValidateOrFn<TParentData, TName, TData>,
+  TOnSubmit extends undefined | FieldValidateOrFn<TParentData, TName, TData>,
+  TOnSubmitAsync extends
+    | undefined
+    | FieldAsyncValidateOrFn<TParentData, TName, TData>,
+  TFormOnMount extends undefined | FormValidateOrFn<TParentData>,
+  TFormOnChange extends undefined | FormValidateOrFn<TParentData>,
+  TFormOnChangeAsync extends undefined | FormAsyncValidateOrFn<TParentData>,
+  TFormOnBlur extends undefined | FormValidateOrFn<TParentData>,
+  TFormOnBlurAsync extends undefined | FormAsyncValidateOrFn<TParentData>,
+  TFormOnSubmit extends undefined | FormValidateOrFn<TParentData>,
+  TFormOnSubmitAsync extends undefined | FormAsyncValidateOrFn<TParentData>,
+  TFormOnServer extends undefined | FormAsyncValidateOrFn<TParentData>,
+  TSubmitMeta,
+> implements OnInit
 {
   name = input.required<TName>()
   defaultValue = input<NoInfer<TData>>()
@@ -127,7 +130,37 @@ export class TanStackField<
       >
     >()
 
+  mode = input<'value' | 'array'>()
+
   disableErrorFlat = input<boolean>()
+
+  _api = computed(() => {
+    return new FieldApi(untracked(this.options))
+  })
+
+  get api(): FieldApi<
+    TParentData,
+    TName,
+    TData,
+    TOnMount,
+    TOnChange,
+    TOnChangeAsync,
+    TOnBlur,
+    TOnBlurAsync,
+    TOnSubmit,
+    TOnSubmitAsync,
+    TFormOnMount,
+    TFormOnChange,
+    TFormOnChangeAsync,
+    TFormOnBlur,
+    TFormOnBlurAsync,
+    TFormOnSubmit,
+    TFormOnSubmitAsync,
+    TFormOnServer,
+    TSubmitMeta
+  > {
+    return this._api()
+  }
 
   options = computed(
     () =>
@@ -164,43 +197,47 @@ export class TanStackField<
       >,
   )
 
-  api!: FieldApi<
-    TParentData,
-    TName,
-    TData,
-    TOnMount,
-    TOnChange,
-    TOnChangeAsync,
-    TOnBlur,
-    TOnBlurAsync,
-    TOnSubmit,
-    TOnSubmitAsync,
-    TFormOnMount,
-    TFormOnChange,
-    TFormOnChangeAsync,
-    TFormOnBlur,
-    TFormOnBlurAsync,
-    TFormOnSubmit,
-    TFormOnSubmitAsync,
-    TFormOnServer,
-    TSubmitMeta
-  >
+  injector = inject(Injector)
 
-  unmount?: () => void
+  constructor() {
+    effect((onCleanup) => {
+      const unmount = this._api().mount()
+
+      onCleanup(() => {
+        unmount()
+      })
+    })
+
+    effect(() => {
+      this._api().update(this.options())
+    })
+  }
+
+  cd = inject(ChangeDetectorRef)
 
   ngOnInit() {
-    this.api = new FieldApi(this.options())
+    const vals = injectStore(
+      this._api().store,
+      this.mode() === 'array'
+        ? (state) => {
+            return [
+              state.meta,
+              Object.keys((state.value as unknown) ?? []).length,
+            ]
+          }
+        : undefined,
+      {
+        injector: this.injector,
+      },
+    )
 
-    this.unmount = this.api.mount()
-  }
-
-  ngOnDestroy() {
-    this.unmount?.()
-  }
-
-  ngOnChanges() {
-    const api = this.api as typeof this.api | undefined
-    if (!api) return
-    api.update(this.options())
+    effect(
+      () => {
+        // Load bearing change detection check
+        const _values = vals()
+        this.cd.markForCheck()
+      },
+      { injector: this.injector },
+    )
   }
 }
--- packages/angular-form/tests/test-setup.ts
+++ packages/angular-form/tests/test-setup.ts
@@ -1,14 +1,19 @@
-import "zone.js"
 import '@analogjs/vitest-angular/setup-snapshots'
 import '@testing-library/jest-dom/vitest'
 
+import { NgModule, provideZonelessChangeDetection } from '@angular/core'
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing'
 import { getTestBed } from '@angular/core/testing'
 
+@NgModule({
+  providers: [provideZonelessChangeDetection()],
+})
+export class ZonelessTestModule {}
+
 getTestBed().initTestEnvironment(
-  [BrowserDynamicTestingModule],
+  [BrowserDynamicTestingModule, ZonelessTestModule],
   platformBrowserDynamicTesting(),
 )
