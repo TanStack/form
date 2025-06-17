b131f70c2a42eef3efaa780d682ec38fb68e70f3
Corbin Crutchley <git@crutchcorn.dev>
2025-06-17 02:01:41 -0700

    chore: add back Zone.js to Test Setup

--- packages/angular-form/tests/test-setup.ts
+++ packages/angular-form/tests/test-setup.ts
@@ -1,19 +1,14 @@
+import "zone.js"
 import '@analogjs/vitest-angular/setup-snapshots'
 import '@testing-library/jest-dom/vitest'
 
-import { NgModule, provideZonelessChangeDetection } from '@angular/core'
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing'
 import { getTestBed } from '@angular/core/testing'
 
-@NgModule({
-  providers: [provideZonelessChangeDetection()],
-})
-export class ZonelessTestModule {}
-
 getTestBed().initTestEnvironment(
-  [BrowserDynamicTestingModule, ZonelessTestModule],
+  [BrowserDynamicTestingModule],
   platformBrowserDynamicTesting(),
 )
