cd19b18d6c03dde736715b0165b54c91a0b025f5
Corbin Crutchley <git@crutchcorn.dev>
2025-08-03 01:07:14 -0700

    chore: fix problems via export

--- packages/solid-form/src/index.tsx
+++ packages/solid-form/src/index.tsx
@@ -2,10 +2,7 @@ export * from '@tanstack/form-core'
 
 export { useStore } from '@tanstack/solid-store'
 
-export { createForm, type SolidFormApi } from './createForm'
-
-export type { CreateField, FieldComponent } from './createField'
-export { createField, Field } from './createField'
-
-export type { WithFormProps } from './createFormHook'
-export { createFormHook, createFormHookContexts } from './createFormHook'
+export * from './createField'
+export * from './createForm'
+export * from './createFormHook'
+export * from './types'
