b7ebd69c100bc35fd1b6448857346647dee0ffef
Harry Whorlow <whorlowharry@gmail.com>
2025-09-17 14:53:33 +0200

    fix: knip

--- packages/react-form-devtools/vite.config.ts
+++ packages/react-form-devtools/vite.config.ts
@@ -19,7 +19,7 @@ const config = defineConfig({
 export default mergeConfig(
   config,
   tanstackViteConfig({
-    entry: ['./src/index.ts'],
+    entry: ['./src/index.ts', './src/production.ts'],
     srcDir: './src',
     cjs: false,
   }),
