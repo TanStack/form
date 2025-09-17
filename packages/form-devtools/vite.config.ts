7874be164bdd22e9fa0b1ca5671bc34507de269e
Harry Whorlow <whorlowharry@gmail.com>
2025-09-17 11:18:43 +0200

    feat: add production build

--- packages/form-devtools/vite.config.ts
+++ packages/form-devtools/vite.config.ts
@@ -19,7 +19,7 @@ const config = defineConfig({
 export default mergeConfig(
   config,
   tanstackViteConfig({
-    entry: ['./src/index.tsx'],
+    entry: ['./src/index.ts'],
     srcDir: './src',
     cjs: false,
   }),
