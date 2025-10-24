b3cfb9e09db932a6984edf7e4cd38fa9c7aea1df
Harry Whorlow <whorlowharry@gmail.com>
2025-10-24 12:50:01 +0200

    feat(workspace): add composite monorepo

--- packages/form-core/vite.config.ts
+++ packages/form-core/vite.config.ts
@@ -1,8 +1,29 @@
 import { defineConfig, mergeConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
 import { tanstackViteConfig } from '@tanstack/config/vite'
 import packageJson from './package.json'
 
 const config = defineConfig({
+  plugins: [
+    tsconfigPaths({
+      projects: ['./tsconfig.prod.json'],
+    }),
+  ],
+
+  // fix from https://github.com/vitest-dev/vitest/issues/6992#issuecomment-2509408660
+  resolve: {
+    conditions: ['@tanstack/custom-condition'],
+  },
+  environments: {
+    ssr: {
+      resolve: {
+        conditions: ['@tanstack/custom-condition'],
+      },
+    },
+  },
+
+  // tests
   test: {
     name: packageJson.name,
     dir: './tests',
