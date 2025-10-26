d3b1481d760663e000970d3d1f8c44b5cef8b267
LeCarbonator <18158911+LeCarbonator@users.noreply.github.com>
2025-10-26 20:46:55 +0100

    chore: fix PR

--- packages/react-form/src/createFormHook.tsx
+++ packages/react-form/src/createFormHook.tsx
@@ -555,7 +555,7 @@ export function createFormHook<
     TOnServer extends undefined | FormAsyncValidateOrFn<TFormData>,
     TSubmitMeta,
   >(
-    props: FormOptions<
+    _props: FormOptions<
       TFormData,
       TOnMount,
       TOnChange,
