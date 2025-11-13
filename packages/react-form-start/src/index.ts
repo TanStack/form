d3883e9c408aff97a24b7066145b6dea41ae304a
Leonardo Montini <lion.48m@gmail.com>
2025-11-13 10:46:25 -0800

    feat: update start to rc (#1771)
    Co-authored-by: Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
    Co-authored-by: Harry Whorlow <whorlowharry@gmail.com>
    Co-authored-by: Harry Whorlow <79278353+harry-whorlow@users.noreply.github.com>
    Co-authored-by: Corbin Crutchley <git@crutchcorn.dev>

--- packages/react-form-start/src/index.ts
+++ packages/react-form-start/src/index.ts
@@ -1,4 +1,6 @@
+export * from '@tanstack/react-form'
+
 export * from './createServerValidate'
 export * from './getFormData'
 export * from './error'
-export * from './types'
+export * from './useTransform'
