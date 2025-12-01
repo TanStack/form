73eeabb4a442db571b99ddd78abb2fb5370300f8
fuko <43729152+fulopkovacs@users.noreply.github.com>
2025-12-01 13:57:33 +0100

    docs: update the large-form example to work with async validators (#1777)

--- examples/react/large-form/src/features/people/shared-form.tsx
+++ examples/react/large-form/src/features/people/shared-form.tsx
@@ -17,4 +17,29 @@ export const peopleFormOpts = formOptions({
       phone: '',
     },
   },
+  validators: {
+    onChangeAsync: async ({ value }) => {
+      const errors = {
+        fields: {},
+      } as {
+        fields: Record<string, string>
+      }
+      if (!value.fullName) {
+        errors.fields.fullName = 'Full name is required'
+      }
+      if (!value.phone) {
+        errors.fields.phone = 'Phone is required'
+      }
+      if (!value.emergencyContact.fullName) {
+        errors.fields['emergencyContact.fullName'] =
+          'Emergency contact full name is required'
+      }
+      if (!value.emergencyContact.phone) {
+        errors.fields['emergencyContact.phone'] =
+          'Emergency contact phone is required'
+      }
+
+      return errors
+    },
+  },
 })
