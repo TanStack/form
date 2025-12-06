34b41e6a65a3ba56c6fb44ec2802a81db757f9c9
Menachem Hornbacher <mhornbacher@kiddom.co>
2025-12-06 17:25:04 -0500

    import correctly

--- packages/react-form/src/useFormId.ts
+++ packages/react-form/src/useFormId.ts
@@ -1,5 +1,5 @@
-import { version as reactVersion, useId as useReactId } from 'react'
+import * as React from 'react'
 import { uuid } from '@tanstack/form-core'
 
 /** React 17 does not have the useId hook, so we use a random uuid as a fallback. */
-export const useFormId = reactVersion.split('.')[0] === '17' ? uuid : useReactId
+export const useFormId = React.version.split('.')[0] === '17' ? uuid : React.useId
