802eb79affb9a7ff7c1ca5e0e5b962fe0d34f55e
Corbin Crutchley <git@crutchcorn.dev>
2026-01-29 21:45:42 -0800

    test: add React unit test

--- packages/react-form/tests/useForm.test.tsx
+++ packages/react-form/tests/useForm.test.tsx
@@ -2,8 +2,8 @@ import { describe, expect, it, vi } from 'vitest'
 import { render, waitFor } from '@testing-library/react'
 import { userEvent } from '@testing-library/user-event'
 import { useStore } from '@tanstack/react-store'
-import { useEffect, useState } from 'react'
-import { useForm } from '../src/index'
+import { useCallback, useEffect, useState } from 'react'
+import { mergeForm, useForm } from '../src/index'
 import { sleep } from './utils'
 
 const user = userEvent.setup()
@@ -1002,5 +1002,39 @@ describe('useForm', () => {
 
     await user.type(input, 'a')
     await user.click(removeButton)
+  })
+
+  it('should handle stable transforms to update the base form on first render', async () => {
+    let renders = 0
+    function Comp() {
+      const form = useForm({
+        defaultValues: {
+          test: 'Hello',
+        },
+        transform: useCallback((baseForm: unknown) => {
+          return mergeForm(baseForm as never, {
+            values: {
+              test: 'What',
+            },
+          })
+        }, []),
+      })
+
+      renders++
+
+      return (
+        <form.Field
+          name="test"
+          children={(field) => (
+            <p>
+              {field.state.value} {renders}
+            </p>
+          )}
+        />
+      )
+    }
+
+    const { getByText } = render(<Comp />)
+    getByText('What 1')
   })
 })
