97f02866d7a88063ce1ae8ae71a02eba8e2a85d2
Corbin Crutchley <git@crutchcorn.dev>
2026-05-09 15:52:21 -0400

    chore: initial large-form example for Lit

---
+++ examples/lit/large-form/src/features/people/emergency-contact.ts
@@ -0,0 +1,38 @@
+import { LitElement, html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+import { getFormType } from '@tanstack/lit-form'
+import { peopleFormOpts } from './shared-form.js'
+import '../../components/text-field.js'
+
+const formType = getFormType(peopleFormOpts)
+
+@customElement('emergency-contact-fields')
+export class EmergencyContactFields extends LitElement {
+  @property({ attribute: false })
+  form!: typeof formType
+
+  protected createRenderRoot() {
+    return this
+  }
+
+  render() {
+    return html`
+      ${this.form.field(
+        { name: 'emergencyContact.fullName' },
+        (field) =>
+          html`<text-field label="Full Name" .field=${field}></text-field>`,
+      )}
+      ${this.form.field(
+        { name: 'emergencyContact.phone' },
+        (field) =>
+          html`<text-field label="Phone" .field=${field}></text-field>`,
+      )}
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'emergency-contact-fields': EmergencyContactFields
+  }
+}
