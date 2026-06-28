aa241c7ad1da652df7388b87dd9808c0bcc54041
LeCarbonator <18158911+LeCarbonator@users.noreply.github.com>
2026-06-28 14:12:37 +0200

    oops, fix build issues

--- packages/react-form-devtools/src/plugin.tsx
+++ packages/react-form-devtools/src/plugin.tsx
@@ -3,13 +3,19 @@
 import { createReactPlugin } from '@tanstack/devtools-utils/react'
 import { FormDevtoolsPanel } from './FormDevtools'
 
+import type { TanStackDevtoolsPlugin } from '@tanstack/devtools'
 import type { DevtoolsPanelProps } from '@tanstack/devtools-utils/react'
+
+type ReactFormDevtoolsPlugin = () => TanStackDevtoolsPlugin
 
 function ReactFormDevtoolsPanel(props: DevtoolsPanelProps) {
   return <FormDevtoolsPanel {...props} adapterName="React" />
 }
 
-const [formDevtoolsPlugin, formDevtoolsNoOpPlugin] = createReactPlugin({
+const [formDevtoolsPlugin, formDevtoolsNoOpPlugin]: readonly [
+  ReactFormDevtoolsPlugin,
+  ReactFormDevtoolsPlugin,
+] = createReactPlugin({
   name: 'TanStack Form',
   Component: ReactFormDevtoolsPanel,
 })
--- packages/react-form-devtools/src/production/plugin.tsx
+++ packages/react-form-devtools/src/production/plugin.tsx
@@ -3,13 +3,19 @@
 import { createReactPlugin } from '@tanstack/devtools-utils/react'
 import { FormDevtoolsPanel } from './FormDevtools'
 
+import type { TanStackDevtoolsPlugin } from '@tanstack/devtools'
 import type { DevtoolsPanelProps } from '@tanstack/devtools-utils/react'
+
+type ReactFormDevtoolsPlugin = () => TanStackDevtoolsPlugin
 
 function ReactFormDevtoolsPanel(props: DevtoolsPanelProps) {
   return <FormDevtoolsPanel {...props} adapterName="React" />
 }
 
-const [formDevtoolsPlugin] = createReactPlugin({
+const [formDevtoolsPlugin]: readonly [
+  ReactFormDevtoolsPlugin,
+  ReactFormDevtoolsPlugin,
+] = createReactPlugin({
   name: 'TanStack Form',
   Component: ReactFormDevtoolsPanel,
 })
