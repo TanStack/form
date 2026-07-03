211853d9c9919b4c227dded7126348175b8bbda9
LeCarbonator <18158911+LeCarbonator@users.noreply.github.com>
2026-07-03 15:49:15 +0200

    add refresher for form selector

--- packages/form-devtools/src/components/Header.tsx
+++ packages/form-devtools/src/components/Header.tsx
@@ -1,30 +1,45 @@
 import { Header as UIHeader } from '@tanstack/devtools-ui'
-import { InfoIcon } from 'lucide-solid'
+import { RefreshCcwIcon } from 'lucide-solid'
 import { FormLogo } from './ui/FormLogo'
 import { FormSelector } from './FormSelector'
 import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip'
 import { Button } from './ui/button'
 import { Code } from './ui/code'
+import { formDevtoolsEventClient } from '@/eventClient.lib'
 
 interface CryptidNameHintProps {
   class?: string
 }
 
 function CrypticNameHint(props: CryptidNameHintProps) {
+  function handleRefreshClick() {
+    formDevtoolsEventClient.emit('request-mounted-forms', {})
+  }
+
+  // TODO make it a button group with the
   return (
-    <Tooltip>
+    <Tooltip openDelay={400}>
       <TooltipTrigger
         asChild={(innerProps) => (
-          <Button variant="ghost" {...innerProps({ class: props.class })}>
-            <InfoIcon />
+          <Button
+            variant="ghost"
+            {...innerProps({
+              class: props.class,
+              onClick: handleRefreshClick,
+            })}
+          >
+            <RefreshCcwIcon />
           </Button>
         )}
       />
       <TooltipContent class="text-center">
         <span>
-          Form names looking a bit cryptic?
+          Refresh the mounted forms list.
           <br />
-          Add a <Code>formId</Code> option to give it a clearer name.
+          <span class="text-muted-foreground">
+            Form names looking a bit cryptic? Add a <Code>formId</Code> option
+            to give it a clearer name.
+          </span>
         </span>
       </TooltipContent>
     </Tooltip>
