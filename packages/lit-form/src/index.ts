0684b02a6bcd570e64bb82fab0110dabf844b7e8
LeCarbonator <18158911+LeCarbonator@users.noreply.github.com>
2026-08-11 14:31:04 +0200

    [v2] Fix coderabbit and type generation (#2321)
    * add alpha branch to coderabbit coverage
    
    * chore: simplify type reference generation

--- packages/lit-form/src/index.ts
+++ packages/lit-form/src/index.ts
@@ -1,26 +1,5 @@
 export * from '@tanstack/form-core'
 
-export { getFormType } from './get-form-type.js'
-export type { LitFormType } from './get-form-type.js'
-export { TanStackFormController } from './tanstack-form-controller.js'
-export type {
-  LitFieldMethods,
-  LitFormGroupApi,
-  LitSubscribeMethod,
-} from './tanstack-form-controller.js'
-export { defineFieldGroup } from './with-fields.js'
-export type {
-  AnyFieldGroupFieldSlot,
-  DefineFieldGroupFn,
-  FieldGroupDefinition,
-  FieldGroupFieldBindings,
-  FieldGroupFieldBindingsOf,
-  FieldGroupFieldData,
-  FieldGroupFieldSlot,
-  FieldGroupFields,
-  FieldGroupFieldsOf,
-  FieldGroupHelper,
-  FieldGroupWithFieldsFn,
-  LitFieldGroup,
-  LitFieldGroupApi,
-} from './with-fields.js'
+export * from './get-form-type.js'
+export * from './tanstack-form-controller.js'
+export * from './with-fields.js'
